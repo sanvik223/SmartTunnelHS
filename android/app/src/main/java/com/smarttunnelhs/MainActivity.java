4cffe191482fb126288aaeca688763cb9d72b80d
sanvik223 <akramytbd401@gmail.com>
2025-05-31 19:12:07 +0200

    Update MainActivity.java

--- android/app/src/main/java/com/smarttunnelhs/MainActivity.java
+++ android/app/src/main/java/com/smarttunnelhs/MainActivity.java
@@ -3,6 +3,7 @@ package com.smarttunnelhs;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
   @Override
   protected String getMainComponentName() {
     return "SmartTunnelHS";
