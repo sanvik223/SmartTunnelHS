45ff43a43e8d71dff1269894f6f64d90df44127d
sanvik223 <akramytbd401@gmail.com>
2025-05-31 17:47:07 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,34 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// Screens
 import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import HostDashboard from './screens/HostDashboard';
 import ClientDashboard from './screens/ClientDashboard';
 import ProxyConfigScreen from './screens/ProxyConfigScreen';
-import HostQRCodeScreen from './screens/HostQRCodeScreen';
 import ClientQRScannerScreen from './screens/ClientQRScannerScreen';
+import HostQRCodeScreen from './screens/HostQRCodeScreen';
 import ClientControl from './screens/ClientControl';
 
-const Stack = createNativeStackNavigator();
+import './firebaseConfig';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="HostDashboard" component={HostDashboard} />
         <Stack.Screen name="ClientDashboard" component={ClientDashboard} />
         <Stack.Screen name="ProxyConfig" component={ProxyConfigScreen} />
-        <Stack.Screen name="HostQR" component={HostQRCodeScreen} />
         <Stack.Screen name="ClientQRScanner" component={ClientQRScannerScreen} />
+        <Stack.Screen name="HostQR" component={HostQRCodeScreen} />
         <Stack.Screen name="ClientControl" component={ClientControl} />
       </Stack.Navigator>
     </NavigationContainer>
